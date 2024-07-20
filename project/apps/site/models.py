c53ae4ae931f3d9ba593ace89cfcbd6a56276dda
mrf0rtuna4 <majned90@gmail.com>
2024-07-20 14:57:47 +0300

    fix!: app label

--- project/apps/site/models.py
+++ project/apps/site/models.py
@@ -3,20 +3,29 @@ import markdown
 from django.db import models
 from django.utils.safestring import mark_safe
 
+
 class Information(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+
+    class Meta:
+        app_label = 'site'
 
     def get_markdown_content(self):
         md = markdown.Markdown(extensions=['extra', 'nl2br'])
         return mark_safe(md.convert(self.content))
 
+
 class DatabaseEntry(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
 
+    class Meta:
+        app_label = 'site'
+
     def __str__(self):
         return self.title
+
 
 class Character(models.Model):
     name = models.CharField(max_length=100)
@@ -24,13 +33,20 @@ class Character(models.Model):
     clearance_level = models.CharField(max_length=100, default='N/A')
     bio = models.TextField()
 
+    class Meta:
+        app_label = 'site'
+
     def get_markdown_bio(self):
         md = markdown.Markdown(extensions=['extra', 'nl2br'])
         return mark_safe(md.convert(self.bio))
 
+
 class Location(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+
+    class Meta:
+        app_label = 'site'
 
     def __str__(self):
         return self.name
