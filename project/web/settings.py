7ab61b44d8680ad830fbe6808139ab3f8b0ed9ac
mrf0rtuna4 <majned90@gmail.com>
2024-07-24 10:44:17 +0300

    try fix!

--- project/web/settings.py
+++ project/web/settings.py
@@ -72,6 +72,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries':{
+                'custom_tags': 'apps.site.templatetags.custom_tags',
+            }
         },
     },
 ]
